46ae04e8b169c410223087b020a2ee39d6d659d3
Pranay <pranay.chahare@hbwsl.com>
2022-02-03 10:00:44 +0530

    Adde gitpod setup

---
+++ setup-module.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+folders=(.github Block Controller etc Helper Model Observer Setup Test view )
+files=(.gitignore composer.json LICENSE README.md registration.php)
+
+for files in "${folders[@]}"; do
+    rm -r "$files"
+done
