546602b5b15eee7e075cc3bf7a2ec41df24baf59
Pranay <pranay.chahare@hbwsl.com>
2022-02-03 10:40:16 +0530

    Added default no to composer install

--- m2-install.sh
+++ m2-install.sh
@@ -15,7 +15,7 @@ $ES_HOME79/bin/elasticsearch -d -p $ES_HOME79/pid -Ediscovery.type=single-node &
 sleep 15;
 
 mysql -u root -padmin@12345 -e 'create database magento2;' &&
-url=$(gp url | awk -F"//" {'print $2'}) && url+="/" && url="https://8002-"$url;cd /workspace/magento2gitpod && composer config -g -a http-basic.repo.magento.com 64229a8ef905329a184da4f174597d25 a0df0bec06011c7f1e8ea8833ca7661e && composer install && php bin/magento setup:install --db-name='magento2' --db-user='root' --db-password='admin@12345' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='admin@12345' --admin-email='pranay.chahare@hbwsl.com' --admin-firstname='Admin' --admin-lastname='Admin' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis'
+url=$(gp url | awk -F"//" {'print $2'}) && url+="/" && url="https://8002-"$url;cd /workspace/magento2gitpod && composer config -g -a http-basic.repo.magento.com 64229a8ef905329a184da4f174597d25 a0df0bec06011c7f1e8ea8833ca7661e && composer install -n && php bin/magento setup:install --db-name='magento2' --db-user='root' --db-password='admin@12345' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='admin@12345' --admin-email='pranay.chahare@hbwsl.com' --admin-firstname='Admin' --admin-lastname='Admin' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis'
 
 n98-magerun2 module:disable Magento_Csp &&
 n98-magerun2 module:disable Magento_TwoFactorAuth &&
