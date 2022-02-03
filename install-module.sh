7c1ecaadd6fc7d3f853f0ca30e266701ae12c99a
Pranay <pranay.chahare@hbwsl.com>
2022-02-03 10:32:48 +0530

    Script for setup and install

---
+++ install-module.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /workspace/magento2gitpod/app && mkdir -p code/Humcommerce && cd code/Humcommerce && git clone https://github.com/pranayhbwsl/magento2gitpod.git && mv magento2gitpod LegalPages
